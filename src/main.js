42cc4e86f1343b74d677c5e5e57d21edabc3e7dd
Achim Friedland <achim.friedland@graphdefined.com>
2021-01-30 11:48:50 +0100

    Some charging station -> backend commands added

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width:  1200,
+    height:  900,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
@@ -24,6 +24,7 @@ function createWindow () {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
+  
   createWindow()
   
   app.on('activate', function () {
@@ -31,6 +32,7 @@ app.whenReady().then(() => {
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
+
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
